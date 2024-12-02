201165d958a7d302d37f4987750908f31ecd4062
Tade Strehk <git@strehk.eu>
2024-12-02 12:49:38 +0100

    Improved Task 1

--- 2024/1.py
+++ 2024/1.py
@@ -7,32 +7,9 @@ left_list = [int(x.split("   ")[0].strip()) for x in lines]
 right_list = [int(x.split("   ")[1].strip()) for x in lines]
 
 # Task 1
-
-distances = []
-
-left_list_copy = left_list.copy()
-right_list_copy = right_list.copy()
-
-while len(left_list_copy) > 0:
-    left_min = min(left_list_copy)
-    right_min = min(right_list_copy)
-    
-    left_index = left_list_copy.index(left_min)
-    right_index = right_list_copy.index(right_min)
-    
-    distances.append(abs(left_min - right_min))
-    
-    left_list_copy.pop(left_index)
-    right_list_copy.pop(right_index)
-
+distances = [abs(x - y) for x, y in zip(sorted(left_list), sorted(right_list))]
 print(sum(distances))
 
 # Task 2
-
-similarity_score = 0
-
-for num in left_list:
-    counter = Counter(right_list)
-    similarity_score += num * counter[num]
-    
+similarity_score = sum(num * right_list.count(num) for num in left_list)
 print(similarity_score)
